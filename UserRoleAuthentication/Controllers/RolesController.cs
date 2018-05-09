099bf168ae9c4217f07e3617f2ff52dc506f7b63
Gowtham <gowthamk@nousinfo.com>
2018-05-09 11:30:22 +0530

    Add project files.

---
+++ UserRoleAuthentication/Controllers/RolesController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UserRoleAuthentication.Models;
+
+namespace UserRoleAuthentication.Controllers
+{
+    public class RolesController : Controller
+    {
+        ApplicationDbContext context;
+
+        public RolesController()
+        {
+            context = new ApplicationDbContext();
+        }
+
+
+
+        /// <summary>
+        /// Get All Roles
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Index()
+        {
+            var Roles = context.Roles.ToList();
+            return View(Roles);
+        }
+
+        /// <summary>
+        /// Create  a New role
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Create()
+        {
+            var Role = new IdentityRole();
+            return View(Role);
+        }
+
+        /// <summary>
+        /// Create a New Role
+        /// </summary>
+        /// <param name="Role"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult Create(IdentityRole Role)
+        {
+            context.Roles.Add(Role);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+    }
+}
+}
---
+++ UserRoleAuthentication/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="UserRoleAuthentication.MvcApplication" Language="C#" %>
